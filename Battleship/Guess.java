bdfbe5da93bd5d7f741132dd995c7692efe54081
Ameya S <55192907+parsec1310@users.noreply.github.com>
2020-07-19 00:23:19 -0400

    Add files via upload

---
+++ Battleship/Guess.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Battleship;
+
+/**
+ *
+ * @author ameya
+ */
+public class Guess {
+    
+    public int r;
+    public int c;
+
+    public Guess(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+    
+    
+    
+}
---
+++ Battleship/Player.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Battleship;
+
+/**
+ *
+ * @author ameya
+ */
+public class Player{
+    public ShipBoard shipBoard;
+    public GuessBoard guessBoard;
+    public String name;
+    
+    
+    public Player(String name){
+        this.name = name;
+        guessBoard = new GuessBoard();
+        shipBoard = new ShipBoard();
+    }
+}
